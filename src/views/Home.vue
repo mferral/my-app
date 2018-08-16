67ad4840f383e60a72240e5ca4da6ddd75d0612d
Miguel Ferral <mferral@gmail.com>
2018-08-16 12:55:21 -0500

    Welcome Prros

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App Prrs"/>
+    <HelloWorld msg="Bienvenidos a Vue.js App Prrs"/>
   </div>
 </template>
 
